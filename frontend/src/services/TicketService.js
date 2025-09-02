c2e54ab99a11a8a57ed6e804f08075f9b8b8a8f6
Rijad-Ismailovic <rijad.isma@gmail.com>
2025-09-02 13:54:52 +0200

    changed routes

--- frontend/src/services/TicketService.js
+++ frontend/src/services/TicketService.js
@@ -1,6 +1,7 @@
 import api, { authApi } from "./axiosConfig";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/tickets";
+//const REST_API_BASE_URL = "http://localhost:8080/api/tickets";
+const REST_API_BASE_URL = "https://diplomski-35765e9c137c.herokuapp.com/api";
 
 export const reserveTicket = (payload) =>
   authApi().post(`${REST_API_BASE_URL}`, payload);
