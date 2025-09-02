4da2ce145b30476847a5969456b4896bfd39df49
Rijad-Ismailovic <rijad.isma@gmail.com>
2025-09-02 13:44:18 +0200

    changed ajax api path

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import api, { authApi } from "./axiosConfig";
 
-const REST_API_BASE_URL = "http://localhost:8080/api";
+// const REST_API_BASE_URL = "http://localhost:8080/api";
+const REST_API_BASE_URL = "https://diplomski-35765e9c137c.herokuapp.com/api";
+
 
 export const register = (payload) =>
   axios.post(REST_API_BASE_URL + "/register", payload);
--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  //baseURL: "http://localhost:8080/api",
+  baseURL: "https://diplomski-35765e9c137c.herokuapp.com/api",
 });
 
 export const authApi = () => {
   const token = localStorage.getItem("jwt");
   return axios.create({
-    baseURL: "http://localhost:8080/api",
+    //baseURL: "http://localhost:8080/api",
+    baseURL: "https://diplomski-35765e9c137c.herokuapp.com/api",
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
     },
